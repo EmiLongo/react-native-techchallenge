ba5fa8203f646b49f09cfece039d4ffae5d30700
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2024-11-19 15:41:01 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { getLatestGames } from './lib/metacritic';
 
 export default function App() {
+  const [games , setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    })
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+
+        {games?.map((game) => {
+          return (
+            <View key={game.slug}>
+              <Image source={{ uri: game.image }} style={{ width: 200, height: 200 }} />
+              <Text>{game.title}</Text>
+              <Text>{game.score}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </>
   );
 }
 
